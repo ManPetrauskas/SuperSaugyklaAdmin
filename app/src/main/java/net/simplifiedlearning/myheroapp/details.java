b76815099f2c7cfc6657ffc83c8f7978d54bff9c
GedSelenis <selenisgediminas5@gmail.com>
2019-07-25 21:47:03 +0300

    yeet

--- app/src/main/java/net/simplifiedlearning/myheroapp/details.java
+++ app/src/main/java/net/simplifiedlearning/myheroapp/details.java
@@ -1,6 +1,7 @@
 package net.simplifiedlearning.myheroapp;
 
 import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,16 +9,98 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+
+import static android.view.View.GONE;
 
 public class details extends AppCompatActivity {
 
+    List<TableChanges> tableList;
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+        tableList = new ArrayList<>();
+        listView = (ListView) findViewById(R.id.listViewHeroes);
+    }
+    private void refreshHeroList(JSONArray heroes) throws JSONException {
+//        heroList.clear();
+        tableList.clear();
+
+        for (int i = 0; i < heroes.length(); i++) {
+            JSONObject obj = heroes.getJSONObject(i);
+
+            tableList.add(new TableChanges(
+                    obj.getInt("id"),
+                    obj.getInt("user_id"),
+                    obj.getString("first_name"),
+                    obj.getString("last_name"),
+                    obj.getString("login_token"),
+                    obj.getString("last_time_started"),
+                    obj.getString("last_time_ended"),
+                    obj.getLong("todays_worktime")
+            ));
+        }
+
+        details.HeroAdapter adapter = new details.HeroAdapter(tableList);
+        listView.setAdapter(adapter);
+    }
+    private class PerformNetworkRequest extends AsyncTask<Void, Void, String> {
+        String url;
+        HashMap<String, String> params;
+        int requestCode;
+
+        PerformNetworkRequest(String url, HashMap<String, String> params, int requestCode) {
+            this.url = url;
+            this.params = params;
+            this.requestCode = requestCode;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+//            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+//            progressBar.setVisibility(GONE);
+            try {
+                JSONObject object = new JSONObject(s);
+                if (!object.getBoolean("error")) {
+                    Toast.makeText(getApplicationContext(), object.getString("message"), Toast.LENGTH_SHORT).show();
+                    refreshHeroList(object.getJSONArray("heroes"));
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            RequestHandler requestHandler = new RequestHandler();
+
+            if (requestCode == CODE_POST_REQUEST)
+                return requestHandler.sendPostRequest(url, params);
+
+
+            if (requestCode == CODE_GET_REQUEST)
+                return requestHandler.sendGetRequest(url);
+
+            return null;
+        }
     }
     class HeroAdapter extends ArrayAdapter<TableChanges> {
         List<TableChanges> tableList;
