7b89966bf4bcd9488bee3cf5a3dc02b2b393ebb8
GedSelenis <selenisgediminas5@gmail.com>
2019-08-04 12:10:13 +0300

    yeet

--- app/src/main/java/net/simplifiedlearning/myheroapp/MainActivity.java
+++ app/src/main/java/net/simplifiedlearning/myheroapp/MainActivity.java
@@ -93,9 +93,10 @@ public class MainActivity extends AppCompatActivity {
         buttonFind.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                maching = true;
+                if (!isUpdating){
+                    maching = true;
                 readHeroes();
-
+                }
             }
         });
 
